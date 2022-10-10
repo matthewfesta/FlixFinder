b7b4df46f8581e6a5ca463bf08ef3533f7604651
Matthew Festa <festa.matthew@me.com>
2022-10-10 10:17:13 -0400

    changed api url to https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
 
-const API_URL = 'http://www.omdbapi.com?apikey=9ab56e03'
+const API_URL = 'https://www.omdbapi.com?apikey=9ab56e03'
 
 const movie1 = {
 
